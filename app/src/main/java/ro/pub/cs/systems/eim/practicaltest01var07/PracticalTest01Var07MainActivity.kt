798480a44abbeb1b3740ec0d2c6a7c6e5bee78ae
SicKCristi <cristiscarlatescu20@yahoo.com>
2025-11-09 13:41:52 +0200

    Finalizare A

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07MainActivity.kt
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.practicaltest01var07
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PracticalTest01Var07MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practical_test01_var07_main)
+        }
+    }
