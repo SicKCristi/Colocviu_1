228e00dfa4816b606c9bdd21e40729bcfca113d4
SicKCristi <cristiscarlatescu20@yahoo.com>
2025-11-09 14:10:40 +0200

    Realizare B

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07SecondaryActivity.kt
@@ -0,0 +1,11 @@
+package ro.pub.cs.systems.eim.practicaltest01var07
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PracticalTest01Var07SecondaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary)
+    }
+}
